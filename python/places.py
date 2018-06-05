c6e7c650b7cb96f7740d71a83c57cb631c7fc2d2
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-05 15:55:33 -0400

    hi

--- python/places.py
+++ python/places.py
@@ -8,3 +8,6 @@ print(places_sentences[0])
 print(places_sentences[1])
 print(places_sentences[2])
 print(places_sentences[3])
+
+for place in places:
+    print("I would like to visit " + place + "!")
