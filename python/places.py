696d76bb6399621b629cf1d3045c7f9ea6fa22cc
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-05 15:50:41 -0400

    places

---
+++ python/places.py
@@ -0,0 +1,10 @@
+places=["Mongolia", "Antarctica", "Peru", "Stardew Valley"]
+places_sentences=[]
+for place in places:
+    places_sentences.append("I would like to visit " + place + "!")
+
+places_sentences
+print(places_sentences[0])
+print(places_sentences[1])
+print(places_sentences[2])
+print(places_sentences[3])
