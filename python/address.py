6a79149f8a30d05ad4001d5cc53bd0a07c549b9a
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-05 14:15:00 -0400

    address

---
+++ python/address.py
@@ -0,0 +1,9 @@
+address={"name":"Ashley Chang",
+"street":"1195 Chapel St Apt 3",
+"city":"New Haven",
+"state":"CT",
+"zip":"06511"
+}
+print(address["name"])
+print(address["street"])
+print(address["city"] + ", "+ address["state"] + " " + address["zip"])
