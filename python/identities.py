c5de0283b6733c5f6f38d84f7275ea47520e9405
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-05 16:19:54 -0400

    identities

---
+++ python/identities.py
@@ -0,0 +1,7 @@
+identities={"Amnesia": "forgetful Ashley",
+"Jabba": "greedy Ashley",
+"Poopgirl": "poopy Ashley"
+}
+
+for nickname, Ashley in identities.items():
+    print(nickname + " is the secret identity of " + Ashley + "!!!")
