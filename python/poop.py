0944034063ded726a9b2da1610e8f79e91e75fbd
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-06 09:50:48 -0400

    poo

---
+++ python/poop.py
@@ -0,0 +1,7 @@
+def think_about_poop(destination):
+    print("I want to poop " + destination + "--is that okay?")
+
+destinations=["in utter and complete privacy", "like Hot Carl", "back and forth"]
+
+for destination in destinations:
+    think_about_poop(destination)
