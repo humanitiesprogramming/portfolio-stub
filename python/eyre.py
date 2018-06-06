5b282cd1e24cbcb48ac7e1478f4dd931043c0943
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-06 15:39:37 -0400

    m

--- python/eyre.py
+++ python/eyre.py
@@ -1,19 +1,34 @@
 # There is a package called nltk. Loads it for this file.
 import nltk
 
+def open_file_and_get_text(filename):
+    # Given a filename, opens it.
+    # "r" means read-only.
+    # "our_file" could be "chicken_nugget"
+    with open(filename, "r") as our_file:
+        # Takes the file and reads the text. Stores it.
+        text = our_file.read()
+    return text
+
+# Given some tokens, lowercases them all.
+def clean_tokens(words):
+    # Creates an empty list called clean_words
+    clean_words = []
+    # Loops over each word item in the words list
+    for word in words:
+        # Makes each word item lowercase and appends it to the new list called clean_words
+        clean_words.append(word.lower())
+    return clean_words
+
 # Sets a variable filename for where our file is.
 filename = "eyre.txt"
 
-# Given a filename, opens it.
-# "r" means read-only.
-# "our_file" could be "chicken_nugget"
-with open(filename, "r") as our_file:
-    # Takes the file and reads the text. Stores it.
-    text = our_file.read()
-
-# Prints the first 100 characters, starting from 0 (so the first 99 characters).
-print(text[0:100])
-
+text = open_file_and_get_text(filename)
 #Takes a long string and breaks it into words.
 words = nltk.word_tokenize(text)
-print(words[0:10])
+clean_words = clean_tokens(words)
+print(clean_words[0:30])
+word_counts = nltk.FreqDist(clean_words)
+print(word_counts.most_common(10))
+print(word_counts["jane"])
+nltk.Text(clean_words).dispersion_plot(["he", "she", "jane", "tony"])
