c4c2b3b7e3f5f01eaabcc90923f881a9161b847a
Ashley Chang <ashleychang@vagelos-ve702-0365.apng.wlan.private.upenn.edu>
2018-06-06 10:06:10 -0400

    bucket

---
+++ python/bucket.py
@@ -0,0 +1,8 @@
+people={"Charles Dickens":"1870",
+"William Thackeray":"1863",
+"Anthony Trollope":"1882",
+"Gerard Manley Hopkins":"1889"
+}
+
+for name, year in people.items():
+    print(name + " kicked the bucket in " + year + ".")
